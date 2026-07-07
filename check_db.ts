9492352751b5012551d956bb0ad95a11e85ebc36
Hussnain Ahmad <mrhussnainahmad@gmail.com>
2026-07-07 20:22:44 +0500

    removing unwanted files

--- check_db.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./src/db";
-import { platformPages } from "./src/db/schema";
-
-async function run() {
-  const pages = await db.select().from(platformPages);
-  console.log("Pages in DB:");
-  console.log(pages);
-}
-run();
